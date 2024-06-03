b0ccc6b297eeb5ed316314b35999f3ac312ce24d
saint thiri zaw <saintstz@gmail.com>
2024-06-03 14:22:46 +0800

    changes

--- sqli-demo/server.js
+++ sqli-demo/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'saintstz3005',
+  password: 'root',
   database: 'test_db'
 });
 
